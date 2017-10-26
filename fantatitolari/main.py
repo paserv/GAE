a31adfc2e2d622416db8ec497d31af8633ba41fb
DonPablo <paserv@gmail.com>
2017-10-26 09:32:03 +0200

    logo

--- fantatitolari/main.py
+++ fantatitolari/main.py
@@ -11,10 +11,8 @@ app = Flask(__name__)
 def welcome():
     user = users.get_current_user()
     if user:
-        nickname = user.nickname()
         logout_url = users.create_logout_url('/')
-        greeting = 'Welcome, {}! <br /><a href="{}">sign out</a><br /><a href="admin">Admin</a>'.format(nickname, logout_url)
+        return render_template('home.html', logout_url = logout_url)
     else:
         login_url = users.create_login_url('/')
-        greeting = '<a href="{}">Sign in</a>'.format(login_url)
-    return greeting
+        return render_template('home.html', login_url = login_url)
